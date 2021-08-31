c6281874c840ed91e42b7520ea87752ae3dc6c5d
Rehaan Rafiq <88104332+Rehaanr@users.noreply.github.com>
2021-08-31 18:07:59 +0100

    Add files via upload

--- project1/js/script.js
+++ project1/js/script.js
@@ -1,6 +1,12 @@
-var mymap = L.map('mapid').setView([51.505, -0.09], 13);
+var mymap = L.map('mapid').setView([52.3555, 1.1743], 5);
 
 var Stadia_OSMBright = L.tileLayer('https://tiles.stadiamaps.com/tiles/osm_bright/{z}/{x}/{y}{r}.png', {
 	maxZoom: 20,
 	attribution: '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
-});
+}).addTo(mymap);
+
+var polygon = L.polygon([
+    [55.3781, 3.4360],
+    [51.503, -0.06],
+    [51.51, -0.047]
+]).addTo(mymap);
