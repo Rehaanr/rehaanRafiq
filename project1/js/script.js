c750e10051981adf0cfaa128be6b36d180539dc9
Rehaan Rafiq <88104332+Rehaanr@users.noreply.github.com>
2021-09-02 10:46:46 +0100

    Add files via upload

--- project1/js/script.js
+++ project1/js/script.js
@@ -1,3 +1,4 @@
+// Loading map
 var mymap = L.map('mapid').setView([52.3555, 1.1743], 5);
 
 var Stadia_OSMBright = L.tileLayer('https://tiles.stadiamaps.com/tiles/osm_bright/{z}/{x}/{y}{r}.png', {
@@ -5,3 +6,32 @@ var Stadia_OSMBright = L.tileLayer('https://tiles.stadiamaps.com/tiles/osm_brigh
 	attribution: '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
 }).addTo(mymap);
 
+// Loading data using ajax 
+
+    $.ajax({
+        url: "php/GetAllCountries.php",
+        type: 'GET',
+        dataType: 'json',
+        data: {
+           
+        
+        },
+
+        success: function(result) {
+            
+            console.log(result);
+
+        if(result.status.name == "ok") {
+            
+            
+
+        }
+
+
+        }
+    })
+
+   
+
+
+        
--- project1/php/GetAllCountries.php
+++ project1/php/GetAllCountries.php
@@ -3,9 +3,23 @@
 $country = $_POST['country'];
 
 //Getting data 
-$data = file_get_contents();
+$data = file_get_contents("data/countryBorders.geo.json");
 
+//Attaching data 
 $data = json_decode($data, true);
+$country_name = $data['name'];
+
+
+
+
+
+
+
+
+//Sending data back to javascript
+header('Content-type: application/json; charset=UTF-8');
+echo json_encode($data);
+
 
 
 
