7c5820a535bcaef522a53035869d4f08952b0920
Rehaan Rafiq <88104332+Rehaanr@users.noreply.github.com>
2021-09-15 15:22:59 +0100

    Add files via upload

---
+++ project1/php/getcountrypolygon.php
@@ -0,0 +1,40 @@
+<?php 
+ini_set('display_errors', 'on');
+error_reporting(E_ALL);
+
+
+
+
+// Getting Country Polygon
+
+function getCountryPolygons($countrySelected){
+$data_as_string = file_get_contents("../data/countryBorders.geo.json");
+$data = json_decode($data_as_string, true);
+    foreach($data['features'] as $country) {
+        if ($country['properties']['iso_a2'] == $countrySelected) {
+        return $country['geometry']['coordinates'];
+        }
+    }
+}
+
+
+$output["status"]["code"] = "200";
+$output["status"]["name"] = "ok";
+$output["status"]["description"] = "success";
+
+// Country Polygons
+$countrySelected = $_REQUEST['selectedCountry'];
+
+$output["countryPolygons"] = getCountryPolygons($countrySelected);
+
+
+
+
+
+//Sending data back to javascript
+header('Content-type: application/json; charset=UTF-8');
+echo json_encode($output);
+
+
+
+?>
