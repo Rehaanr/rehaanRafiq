f4590ef456ac973d59b247efd35a63a7bea36de7
Rehaan Rafiq <88104332+Rehaanr@users.noreply.github.com>
2021-09-15 15:40:47 +0100

    Add files via upload

--- project1/js/script.js
+++ project1/js/script.js
@@ -6,14 +6,13 @@ var Stadia_OSMBright = L.tileLayer('https://tiles.stadiamaps.com/tiles/osm_brigh
 	attribution: '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
 }).addTo(mymap);
 
-// Loading data using ajax 
+// Adding country Lists
 
     $.ajax({
-        url: "../php/getallcountries.php",
+        url: "php/getallcountries.php",
         type: 'GET',
         dataType: 'json',
-        data: {
-        },
+        data: {},
 
         success: function(result) {
         console.log(result);
@@ -21,16 +20,47 @@ var Stadia_OSMBright = L.tileLayer('https://tiles.stadiamaps.com/tiles/osm_brigh
             
             countryList = result['countryList'];
 
-            $('countriesDataList').append($('<option></option>'),{
-                value: countryList['iso_a2'],
-                name: countryList['name']
-            });
-
+            
+                countryList.forEach(country => {
+                    let option = document.createElement("option");
+                    option.classList.add("option");
+                    option.value = country['code'];
+                    option.text = country['name'];
+                    $("#countriesDataList").append(option);
+                    
+                });
         }
     }
     });
 
    
+    // Country Polygons
 
+    $.ajax({
+        url: "php/getcountrypolygon.php",
+        type: "GET",
+        dataType: "json",
+        data: {
+           selectedCountry: $('.option').val()
+        },
+
+        success: function(result){
+            console.log(result);
+            if(result.status.name == "ok") {
+
+                countryPolygon = result["countryPolygons"];
+
+                L.geoJSON(countryPolygon, {
+                    style: {color: "rgba(0, 28, 119, 0.678)", fillColor: "#fff"} 
+                }).addTo(mymap);
+
+                $('.option').append({
+                    value: countryPolygon['properties']['iso_a2'],
+                    selected: countryPolygon['properties']['name']
+                })
+            }
+        }
+
+    });
 
         
