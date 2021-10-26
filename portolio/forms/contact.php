778c0b9bd3b684adcd85c43306af13b6ee19c64a
Rehaan Rafiq <88104332+Rehaanr@users.noreply.github.com>
2021-10-26 13:07:38 +0100

    Add files via upload

--- portolio/forms/contact.php
+++ portolio/forms/contact.php
@@ -1,66 +1,120 @@
 <?php
+
 //Import PHPMailer classes into the global namespace
+
 //These must be at the top of your script, not inside a function
+
 use PHPMailer\PHPMailer\PHPMailer;
+
 use PHPMailer\PHPMailer\SMTP;
+
 use PHPMailer\PHPMailer\Exception;
 
+ 
+
 //Load Composer's autoloader
+
 require 'vendor/autoload.php';
-require 'path/to/PHPMailer/src/Exception.php';
-require 'path/to/PHPMailer/src/PHPMailer.php';
-require 'path/to/PHPMailer/src/SMTP.php';
+
+require 'vendor/phpmailer/phpmailer/src/Exception.php';
+
+require 'vendor/phpmailer/phpmailer/src/PHPMailer.php';
+
+require 'vendor/phpmailer/phpmailer/src/SMTP.php';
+
 //Create an instance; passing `true` enables exceptions
 
+ 
 
 $name = strip_tags($_REQUEST['name']);
+
 $email = strip_tags($_REQUEST['email']);
+
 $message = strip_tags($_REQUEST['message']);
+
 
 
 
 $mail = new PHPMailer(true);
 
+ 
 
 try {
+
     //Server settings
+
     $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+
     $mail->isSMTP();                                            //Send using SMTP
+
     $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
+
     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+
     $mail->Username   = 'noreplyrehaanrafiq@gmail.com';                     //SMTP username
+
     $mail->Password   = 'portfolioSite123';                               //SMTP password
+
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
+
     $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
+ 
+
     //Recipients
+
     $mail->setFrom('noreplyrehaanrafiq@gmail.com', 'Rehaan Rafiq');
-    $mail->addAddress('rehaanrafiq4@gmail.com', '');     //Add a recipient
+
+    $mail->addAddress('noreplyrehaanrafiq@gmail.com');     //Add a recipient
+
     // $mail->addAddress('ellen@example.com');               //Name is optional
+
     // $mail->addReplyTo('info@example.com', 'Information');
+
     $mail->addBCC('rehaanrafiq4@gmail.com');
 
+ 
 
     //Content
+
     $mail->isHTML(true);                     
+
     //Set email format to HTML
 
+ 
+
     $body = "
-    Dear Admin, you have received a message from $name<br/>    
 
-    The message is as follow:<br/>
+    Hi, Rehaan, you have received a message from $name on your portfolio contact page<br/><br/>   
 
-    Name: $name<br/>
-    Email: $email<br/><br/>
-    Message: $message
+ 
+
+    The message is as follow:<br/><br/>
+
+ 
+
+    <b>Name:</b> $name<br/><br/>
+
+    <b>Email:</b> $email<br/><br/>
+
+    <b>Message:</b> $message
+
     ";
 
+ 
 
     $mail->Body    = $body;
+
     $mail->AltBody = strip_tags($body);
 
+ 
+
     $mail->send();
+
     echo 'Message has been sent';
+
 } catch (Exception $e) {
+
     echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+
 }
---
+++ portolio/forms/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'e3c1c7ae214638e4bfe9b8f7e7235b1bacc14afd',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'e3c1c7ae214638e4bfe9b8f7e7235b1bacc14afd',
+            'dev_requirement' => false,
+        ),
+        'phpmailer/phpmailer' => array(
+            'pretty_version' => 'v6.5.1',
+            'version' => '6.5.1.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../phpmailer/phpmailer',
+            'aliases' => array(),
+            'reference' => 'dd803df5ad7492e1b40637f7ebd258fee5ca7355',
+            'dev_requirement' => false,
+        ),
+    ),
+);
